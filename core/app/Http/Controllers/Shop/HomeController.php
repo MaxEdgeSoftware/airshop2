f1f95927e4a902a8fc95229cadcc575cbb3e1395
DevTen <logiccmpl@gmail.com>
2023-02-05 04:40:53 +0100

    little bug fixed

--- core/app/Http/Controllers/Shop/HomeController.php
+++ core/app/Http/Controllers/Shop/HomeController.php
@@ -11,6 +11,7 @@ class HomeController extends Controller
     public function index($domain){
         $domain = $domain.'.'.env('APP_CDN');
         $shop = Shop::where("domain", $domain)->first();
+        return redirect(env("APP_URL")."/seller/{$shop->seller_id}-".slug($shop->name));
         return view("shop.index",[
             "pageTitle" => $shop->name,
         ]);
